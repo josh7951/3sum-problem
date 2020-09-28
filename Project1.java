b31b9e1813dd67c7865fcb9202e63c49c0097216
Josh Mejia <jmejia7951@gmail.com>
2020-09-28 14:36:57 -0700

    reworked. read prompt wrong

--- Project1.java
+++ Project1.java
@@ -15,17 +15,26 @@ public class Project1 {
             int[] arr = new int[itr];
             Scanner s1 = new Scanner(f);
 
-            for(int i = 0; i < arr.length; i++)
+            for(int i = 0; i < arr.length; i++)    
                 arr[i] = s1.nextInt();
                 s1.close();
-                Arrays.sort(arr); //Array Sort is O(nlogn)
+                //Arrays.sort(arr); //Array Sort is O(nlogn)
             return arr;
         }
         catch(Exception e) { return null; }
     }
     public static void main(String[] args){
         int[] numArray = readFile("input1.txt");
-        for(int i = 0; i < numArray.length - 2; i++){
+        System.out.println(Arrays.toString(numArray));
+        int[] addVal = {15, 82, 22, 36, 29, 11, 31, 4, 31};
+        Arrays.sort(addVal);
+        int[] targetVal = {119, 9, -2, 57, 29, 73, 93}; 
+        for(int i = 0; i < addVal.length - 2; i++){
+            int j = i + 1;
+            int k = addVal.length - 1;    
+            int sum = addVal[j] + addVal[k] + addVal[i];
+        }
+        /* for(int i = 0; i < numArray.length - 2; i++){
             int j = i + 1;
             int k = numArray.length - 1;
             while(j < k){
@@ -33,13 +42,13 @@ public class Project1 {
                 int test = 2;
                 if (sum == test)
                     System.out.println(i+":"+ numArray[i] +", "+j+":"+ numArray[j]+", "+k+":"+ numArray[k]);
-                /* if(sum > 0)
+                if(sum > 0)
                     k--;
                 else if(sum < 0)
-                    j++; */
+                    j++;
                 else 
                     System.out.println("No Solution");
-            }
-        }
+            } 
+        } */
     }
 }
