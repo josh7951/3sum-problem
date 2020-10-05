881113d15825783f06ca2cf1c73e4d10b6b2680c
Josh Mejia <jmejia7951@gmail.com>
2020-10-05 11:58:40 -0700

    I THINK this might be Da Wae for O(N^2)

--- Project1.java
+++ Project1.java
@@ -18,31 +18,37 @@ public class Project1 {
             for(int i = 0; i < arr.length; i++)    
                 arr[i] = s1.nextInt();
                 s1.close();
-                //Arrays.sort(arr); //Array Sort is O(nlogn)
             return arr;
         }
         catch(Exception e) { return null; }
     }
+    public static boolean findSum(int arr[], int arrSize, int sum){
+        for(int i = 0; i < arrSize - 2; i++){
+            int j = i + 1;
+            int k = arrSize - 1;
+            while(j < k){
+                if(arr[i] + arr[j] + arr[k] == sum){
+                    System.out.println(sum + " Yes");
+                    return true;
+                }
+                else if(arr[i] + arr[j] + arr[k] < sum)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        System.out.println(sum + " No");
+        return false;
+    }
     public static void main(String[] args){
         int[] numArray = readFile("input1.txt");
-        //int[] addVal = {15, 82, 22, 36, 29, 11, 31, 4, 31};
-        int[] addVal = {1, 2, 3, 0};
-        Arrays.sort(addVal);
-        //int[] targetVal = {119, 9, -2, 57, 29, 73, 93}; 
-        int[] targetVal = {3, 4, 6, 10, 12};
-        for(int i = 0; i < addVal.length - 1; i++){
-            if( i !=0 && addVal[i] == addVal[i -1]) continue;
-            int j = i + 1;
-            int k = addVal.length - 1;
-            int sum = addVal[i] + addVal[j] + addVal[k];
-            for(int m = 0; m < targetVal.length; m++){
-                int sumCheck = targetVal[m];
-                if(sumCheck == sum)
-                    System.out.println(sumCheck + " Yes");
-                else{
-                    System.out.println(sumCheck + " No");
-                }
-            }
+        int[] addArr = {15, 82, 22, 36, 29, 11, 31, 4, 31};
+        int[] targetSum = {119, 9, -2, 57, 29, 73, 93}; 
+        int size = addArr.length;
+        Arrays.sort(addArr);
+        for(int m = 0; m < targetSum.length; m++){
+            int sum = targetSum[m];
+            findSum(addArr, size, sum);        
         }
     }
 }
